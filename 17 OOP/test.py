578273f5ff9876a56089011233265592a8efb3f4
bariulmunshi <bariulsoft@gmail.com>
2026-06-10 01:53:35 +0600

    test added

---
+++ 17 OOP/test.py
@@ -0,0 +1,18 @@
+import math
+
+class QuadraticEq:
+    def __init__(self, a, b, c):
+        d = (b * b) - (4 * a * c)
+        if d < 0:
+            print("Roots are imaginary")
+        else:
+            x1 = (-b + math.sqrt(d)) / (2 * a)
+            x2 = (-b - math.sqrt(d)) / (2 * a)
+            print("X1=%.2f" % x1, "X2=%.2f" % x2)
+            print("Roots are Real")
+
+a = int(input("A= "))
+b = int(input("B= "))
+c = int(input("C= "))
+
+qrdeq = QuadraticEq(a, b, c)
